7a5b380f75d2271b4725d9f877007747d65e5166
wylsomerice <baddassdude@mail.ru>
2022-10-02 22:30:20 +0500

    Добавление, изменение

---
+++ GraphicEditor/GraphicEditor/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GraphicEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(1630, 27);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(137, 51);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(1630, 84);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(137, 51);
+            this.editBtn.TabIndex = 1;
+            this.editBtn.Text = "Edit";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(1630, 141);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(137, 51);
+            this.deleteBtn.TabIndex = 2;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(1630, 704);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(137, 51);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(26, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1589, 728);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(1630, 198);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(137, 51);
+            this.clearBtn.TabIndex = 5;
+            this.clearBtn.Text = "Clear All";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1804, 780);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.addBtn);
+            this.Name = "Form1";
+            this.Text = "GraphicEditor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ GraphicEditor/GraphicEditor/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GraphicEditor
+{
+    public partial class Form1 : Form
+    {
+        int cnt = 0;
+        Point[][] p;
+        bool isMouseDown;
+        int lineCatchIndex;
+        bool isPointFocused;
+        int pointCatchIndex;
+        string mode;
+        bool clearAll;
+
+        public Form1()
+        {
+            //this.DoubleBuffered = true;
+            mode = "Line";
+            pointCatchIndex = -1;
+            lineCatchIndex = -1;
+            InitializeComponent();
+            p = new Point[100][];
+            for(int i = 0; i<100; i++)
+            {
+                p[i] = new Point[2];  
+            }
+            addBtn.Enabled = false;
+
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            addBtn.Enabled = false;
+            editBtn.Enabled = true;
+            deleteBtn.Enabled = true;
+            mode = "Line";
+        }
+
+        private void editBtn_Click(object sender, EventArgs e)
+        {
+            addBtn.Enabled = true;
+            editBtn.Enabled = false;
+            deleteBtn.Enabled = true;
+            mode = "Edit";
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            addBtn.Enabled = true;
+            editBtn.Enabled = true;
+            deleteBtn.Enabled = false;
+            mode = "Delete";
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            if (mode == "Line")
+            {
+                p[cnt][1].X = e.X;
+                p[cnt][1].Y = e.Y;
+                cnt++;
+            }
+            if (mode == "Edit")
+            {
+                p[lineCatchIndex][pointCatchIndex].X = e.X;
+                p[lineCatchIndex][pointCatchIndex].Y = e.Y;
+            }
+            if (mode == "Delete")
+            {
+                p[lineCatchIndex][pointCatchIndex].X = 0;
+                p[lineCatchIndex][pointCatchIndex].Y = 0;
+            }
+            
+            mode = "Line";
+            panel1.Invalidate();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            if(mode == "Line")
+            {
+                p[cnt][0].X = e.X;
+                p[cnt][0].Y = e.Y;
+            }
+            if (mode == "Edit")
+            {
+                p[lineCatchIndex][pointCatchIndex].X = e.X;
+                p[lineCatchIndex][pointCatchIndex].Y = e.Y;
+            }
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                p[cnt][1].X = e.X;
+                p[cnt][1].Y = e.Y;
+                if (mode == "Line")
+                {
+                    p[cnt][1].X = e.X;
+                    p[cnt][1].Y = e.Y;
+                }
+                if (mode == "Edit" && editBtn.Enabled == false)
+                {
+                    p[lineCatchIndex][pointCatchIndex].X = e.X;
+                    p[lineCatchIndex][pointCatchIndex].Y = e.Y;
+                }
+            }
+            else
+            {
+                if (editBtn.Enabled == false)
+                {
+                    mode = "Line";
+                    isPointFocused = false;
+                    pointCatchIndex = -1;
+                    lineCatchIndex = -1;
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        if (Math.Abs(p[i][0].X - e.X) < 5 && Math.Abs(p[i][0].Y - e.Y) < 5)
+                        {
+                            isPointFocused = true;
+                            pointCatchIndex = 0;
+                            lineCatchIndex = i;
+                            mode = "Edit";
+                        }
+                        if (Math.Abs(p[i][1].X - e.X) < 5 && Math.Abs(p[i][1].Y - e.Y) < 5)
+                        {
+                            isPointFocused = true;
+                            pointCatchIndex = 1;
+                            lineCatchIndex = i;
+                            mode = "Edit";
+                        }
+                    }
+                }
+            }
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if(isPointFocused && editBtn.Enabled == false)
+            {
+                g.DrawEllipse(new Pen(Color.Red), p[lineCatchIndex][pointCatchIndex].X - 5, p[lineCatchIndex][pointCatchIndex].Y - 5, 10, 10);
+            }
+
+            for (int i = 0; i < cnt; i++)
+            {
+                g.DrawLine(new Pen(Color.Black), p[i][0].X, p[i][0].Y, p[i][1].X, p[i][1].Y);
+            }
+            if(isMouseDown)
+            {
+                g.DrawLine(new Pen(Color.Black), p[cnt][0].X, p[cnt][0].Y, p[cnt][1].X, p[cnt][1].Y);
+            }
+            /*if(clearAll)
+            {
+                Rectangle rect = this.ClientRectangle;
+                rect.X = 0;
+                rect.Y = 0;
+                rect.Width = panel1.Width;
+                rect.Height = panel1.Height;
+                e.Graphics.DrawRectangle(new Pen(Color.White), rect);
+                g.DrawRectangle(new Pen(Color.White), 0, 0, panel1.Width, panel1.Height);
+                e.Graphics.FillRectangle(new SolidBrush(Color.Black), rect);
+                clearAll = false;
+            }*/
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            /*clearAll = true;
+            panel1.Invalidate();*/
+        }
+    }
+}
